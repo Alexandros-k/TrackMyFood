859f8d545f637bd70874d1c374e2ff9cc83ac46e
Alexandros-k <alkon134@gmail.com>
2020-07-05 23:30:09 +0300

    removed Drawer functionnality. it will re imported in the future

--- app/src/main/java/com/example/alex/myapplication/MainActivity.java
+++ app/src/main/java/com/example/alex/myapplication/MainActivity.java
@@ -1,30 +1,23 @@
 package com.example.alex.myapplication;
 
-import android.content.Intent;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
-
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-
-import com.example.alex.myapplication.Fragments.FoodLibraryFragment;
-import com.example.alex.myapplication.Fragments.MainActivityFragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.NavigationView;
 import com.example.alex.myapplication.Fragments.TodayFoodFragment;
+import com.example.alex.myapplication.Fragments.MainActivityFragment;
+import com.example.alex.myapplication.Fragments.FoodLibraryFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
         TodayFoodFragment.Listener {
 
-    private static final String TAG = "MainActivity";
     public String day;
     private SectionPageAdapter mSectionPageAdapter;
-    private ViewPager mViewPager;
 
     public MainActivity() {
         day = Utility.getCurrentDate();
@@ -37,86 +30,24 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_drawer);
-
-        //toolbar einai panw deksia ta settings
-        //ta settings ta energopoieis apo to menu/nav_drawer.xml
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        //auto einai to toggle switch gia to sidepanel
-     /*   ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();*/
-
-        //auto energopoiei to sidepanel preipei na to kanei uncommentout kai sto layout/activity_nav_drawer.xml
-        /*NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);*/
-
         mSectionPageAdapter = new SectionPageAdapter(getSupportFragmentManager());
-        mViewPager = findViewById(R.id.container);
+        ViewPager mViewPager = findViewById(R.id.container);
         setupViewPager(mViewPager);
         TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
     }
 
     @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.nav_drawer, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.dailyGoal) {
-            startActivity(new Intent(this, DailyGoalActivity.class));
-        } else if (id == R.id.Calendar) {
-            startActivity(new Intent(this, CalendarActivity.class));
-        } else if (id == R.id.info) {
-
-        } else if (id == R.id.meal) {
-
-        }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return true;
     }
 
     private void setupViewPager(ViewPager viewPager) {
-        SectionPageAdapter adapter = new SectionPageAdapter(getSupportFragmentManager());
-        adapter.addFragment(new MainActivityFragment(), "MAIN");
-        adapter.addFragment(new TodayFoodFragment(), "TODAY FOOD");
-        adapter.addFragment(new FoodLibraryFragment(), "FOOD LIBRARY");
-        viewPager.setAdapter(adapter);
+        mSectionPageAdapter.addFragment(new MainActivityFragment(), "MAIN");
+        mSectionPageAdapter.addFragment(new TodayFoodFragment(), "TODAY FOOD");
+        mSectionPageAdapter.addFragment(new FoodLibraryFragment(), "FOOD LIBRARY");
+        viewPager.setAdapter(mSectionPageAdapter);
     }
 }
